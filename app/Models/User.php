eb52f4f4cbe2d57b3557dcf7462087476ea0b8d2
Rahul Sharma <8470314+rahulsharmaah@users.noreply.github.com>
2024-02-27 18:14:00 +0530

    added user views

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,8 @@ class User extends Authenticatable
     {
         return $this->is_admin;
     }
+    public function isUser()
+    {
+        return $this->role === 'user'; 
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AgreementController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -113,3 +114,4 @@ Route::group(['middleware' => 'admin_auth'], function () {
         return view('payment');
     });
 });
+Route::get('/user-dashboard', [DashboardController::class, 'index'])->name('dashboard');
